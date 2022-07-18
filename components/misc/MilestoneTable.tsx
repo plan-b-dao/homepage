62193d2bd0d35daac3b6fcf9ecc6684e91ee4ec6
Edin Hadzovic <contact@edinhadzovic.com>
2022-07-18 14:59:36 +0200

    refactoring milestone

--- components/misc/MilestoneTable.tsx
+++ components/misc/MilestoneTable.tsx
@@ -9,23 +9,23 @@ export const MilestoneTable: React.FC<IMilestoneTableProps> = (props) => {
                 <div className="ml-auto">Founder Privlage</div>
             </div>
             <div className="bg-cultured-dark flex px-2 py-3 text-dark-jungle font-playfair-display">
-                <div><span className="font-bold">Milestone 0:</span> Project Bootstrap</div>
+                <div><span className="font-bold">M0:</span> Project Bootstrap</div>
                 <div className="ml-auto">1 ETH</div>
             </div>
             <div className="bg-cultured-dark flex px-2 py-3 text-dark-jungle font-playfair-display">
-                <div><span className="font-bold">Milestone 1:</span> Plan B Dao Token and creating Uniswap liquidty</div>
+                <div><span className="font-bold">M1:</span> Plan B Dao Token and creating Uniswap liquidty</div>
                 <div className="ml-auto">0.85 ETH</div>
             </div>
             <div className="bg-cultured-dark flex px-2 py-3 text-dark-jungle font-playfair-display">
-                <div><span className="font-bold">Milestone 2:</span> Application release and Trees NFTs </div>
+                <div><span className="font-bold">M2:</span> Application release and Trees NFTs </div>
                 <div className="ml-auto">0.70 ETH</div>
             </div>
             <div className="bg-cultured-dark flex px-2 py-3 text-dark-jungle font-playfair-display">
-                <div><span className="font-bold">Milestone 3:</span> Plan B Coin with custome swap and tokenomics</div>
+                <div><span className="font-bold">M3:</span> Plan B Coin with custome swap and tokenomics</div>
                 <div className="ml-auto">0.40 ETH</div>
             </div>
             <div className="bg-cultured-dark flex px-2 py-3 text-dark-jungle font-playfair-display">
-                <div><span className="font-bold">Milestone 4:</span> Stable Coin and First Crypto-hub Center</div>
+                <div><span className="font-bold">M4:</span> Stable Coin and First Crypto-hub Center</div>
                 <div className="ml-auto">0 ETH</div>
             </div>
         </div>
